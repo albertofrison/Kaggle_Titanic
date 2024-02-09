20e93a8569fb2c63ae2a4be0b205249ed0ddaee7
albertofrison <alberto.frison@gmail.com>
2024-02-09 10:54:59 +0100

    First Commit

---
+++ Script.R
@@ -0,0 +1,17 @@
+################################################################################
+# KAGGLE COMPETITION
+# Titanic - Machine Learning from Disaster
+# Competition URL: https://www.kaggle.com/competitions/titanic
+# Made with ♥︎ by Alberto Frison
+# Created on February 2023
+################################################################################
+
+################################################################################
+# Data
+# Download Data from Competition URL and store it into a Data folder into your R project
+# Study the train.csv file passengers data and make a ML predicting model to predict which passenger will survive in the test.csv file
+################################################################################
+
+
+
+
